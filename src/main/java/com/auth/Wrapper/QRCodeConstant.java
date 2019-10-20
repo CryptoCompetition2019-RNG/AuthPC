ed28b84894895dfb4f4b8e3413b05b7463e2761a
shesl-meow <shesonglin@bytedance.com>
2019-10-20 21:44:00 +0800

    add resources file && QRCode width&height

--- src/main/java/com/auth/Wrapper/QRCodeConstant.java
+++ src/main/java/com/auth/Wrapper/QRCodeConstant.java
@@ -2,7 +2,9 @@ package com.auth.Wrapper;
 
 public interface QRCodeConstant {
 
-    int width = 5000;
-
-    int height = 500;
+//    int width = 5000;
+//
+//    int height = 500;
+	  int width = 67 + 12 * (7 - 1);
+	  int height = 67 + 12 * (7- 1);
 }
